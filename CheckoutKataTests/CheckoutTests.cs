d559334ddc1e00df0f77ba1ff78f6598ea12bbbe
Tim Tyler <timtyler.developer@gmail.com>
2018-01-12 13:07:15 +0000

    Adding test stubs.

---
+++ CheckoutKataTests/CheckoutTests.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CheckoutKataTests
+{
+    [TestClass]
+    public class CheckoutTests
+    {
+        public CheckoutTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        [TestMethod]
+        public void Create_checkout()
+        {
+        }
+
+        [TestMethod]
+        public void Scan_item_at_checkout()
+        {
+        }
+
+        [TestMethod]
+        public void Scan_items_in_bulk_at_checkout()
+        {
+        }
+
+        [TestMethod]
+        public void Enter_item_manually_at_checkout()
+        {
+        }
+
+        [TestMethod]
+        public void Remove_item_at_checkout()
+        {
+        }
+
+        [TestMethod]
+        public void Remove_items_in_bulk_at_checkout()
+        {
+        }
+
+        [TestMethod]
+        public void Get_total_price_at_checkout()
+        {
+        }
+
+        [TestMethod]
+        public void Get_total_offer_savings_at_checkout()
+        {
+        }
+    }
+}
---
+++ CheckoutKataTests/StoreInventoryTests.cs
@@ -0,0 +1,149 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace CheckoutKataTests
+{
+    /// <summary>
+    /// Summary description for StoreInventoryTests
+    /// </summary>
+    [TestClass]
+    public class StoreInventoryTests
+    {
+        public StoreInventoryTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Create_inventory_test()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        [TestMethod]
+        public void Add_product_to_inventory_test()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        [TestMethod]
+        public void Retrieve_product_from_inventory_test()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        [TestMethod]
+        public void Retrieve_product_price_from_inventory_test()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        [TestMethod]
+        public void Retrieve_product_offer_from_inventory_test()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        [TestMethod]
+        public void Check_product_offer_is_valid()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        [TestMethod]
+        public void Update_price_of_item_in_inventory_test()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        [TestMethod]
+        public void Check_product_is_in_inventory_test()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        [TestMethod]
+        public void Update_offer_on_item_in_inventory_test()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        [TestMethod]
+        public void Discontinue_item_in_inventory_test()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+
+        [TestMethod]
+        public void Remove_product_from_inventory_test()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
