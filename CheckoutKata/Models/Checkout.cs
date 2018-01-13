64123087d539636a09fa9fd9a85a745461d2e46a
Tim Tyler <timtyler.developer@gmail.com>
2018-01-13 14:14:39 +0000

    Checkout tests partially written (100% fail).

---
+++ CheckoutKata/Models/Checkout.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CheckoutKata.Models
+{
+    class Checkout : ICheckout
+    {
+        public Checkout()
+        {
+        }
+
+        /// <summary>
+        /// <see cref="ICheckout.Count"/>
+        /// </summary>
+        /// <returns>The number of products scanned.</returns>
+        public int Count()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// <see cref="ICheckout.GetTotalPrice"/>
+        /// </summary>
+        /// <returns>The total price of scanned products including special offers.</returns>
+        public int GetTotalPrice()
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <param name="product"></param>
+        /// <returns></returns>
+        public bool Remove(Product product)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Scan(Product product)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
