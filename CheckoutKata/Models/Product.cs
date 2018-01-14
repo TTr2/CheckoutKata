7006cf71c1508d2afe5f98dd0fe670d39e28798c
Tim Tyler <timtyler.developer@gmail.com>
2018-01-14 17:48:55 +0000

    Added validation of price in Product class, added test in ProductTests.

---
+++ CheckoutKata/Models/Product.cs
@@ -0,0 +1,53 @@
+﻿namespace CheckoutKata.Models
+{
+    using System;
+
+    /// <summary>
+    /// Represents a product consisting of an SKU code, a price and a MultiDeal,
+    /// which can be added to an inventory of products.
+    /// </summary>
+    class Product
+    {
+        /// <summary>
+        /// Constructor for a Product with optional MultiDeal reference 
+        /// (defaults to a non-valid instance).
+        /// </summary>
+        /// <param name="sku">The SKU of the product.</param>
+        /// <param name="price">The price of the product.</param>
+        /// <param name="multiDeal">The (optional) MultiDeal of the product.</param>
+        public Product(string sku, int price, MultiDeal multiDeal = null)
+        {
+            this.Sku = string.IsNullOrEmpty(sku) ? throw new ArgumentNullException(nameof(sku)) : sku;
+            this.Price = price > 1 ? price : throw new ArgumentException("Price must be greater than zero.");
+            this.MultiDeal = multiDeal == null ? new MultiDeal() : multiDeal;
+        }
+
+        /// <summary>
+        /// The SKU of the product.
+        /// </summary>
+        internal string Sku { get; }
+
+        /// <summary>
+        /// The price of the product (100 = 1 decimal unit).
+        /// </summary>
+        internal int Price { get; }
+
+        /// <summary>
+        /// The MultiDeal for the product, default is non-valid.
+        /// </summary>
+        internal MultiDeal MultiDeal {get; }
+
+        /// <summary>
+        /// Whether the given time is after this Product's MultiDeal starts and 
+        /// before it's MultiDeal ends.
+        /// </summary>
+        /// <param name="now">The current time to check against the MultiDeals 
+        /// valid from/before values.</param>
+        /// <returns><see cref="Product.IsMultiDealValid(DateTime)"/></returns>
+        internal bool IsMultiDealValid(DateTime now)
+        {
+            return now > this.MultiDeal.ValidFromDate 
+                && now < this.MultiDeal.ValidBeforeDate;
+        }
+    }
+}
---
+++ CheckoutKataTests/ProductTests.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CheckoutKata.Models;
+
+namespace CheckoutKataTests
+{
+    /// <summary>
+    /// Summary description for ProductTests
+    /// </summary>
+    [TestClass]
+    public class ProductTests
+    {
+        public ProductTests()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        private TestContext testContextInstance;
+
+        /// <summary>
+        ///Gets or sets the test context which provides
+        ///information about and functionality for the current test run.
+        ///</summary>
+        public TestContext TestContext
+        {
+            get
+            {
+                return testContextInstance;
+            }
+            set
+            {
+                testContextInstance = value;
+            }
+        }
+
+        #region Additional test attributes
+        //
+        // You can use the following additional attributes as you write your tests:
+        //
+        // Use ClassInitialize to run code before running the first test in the class
+        // [ClassInitialize()]
+        // public static void MyClassInitialize(TestContext testContext) { }
+        //
+        // Use ClassCleanup to run code after all tests in a class have run
+        // [ClassCleanup()]
+        // public static void MyClassCleanup() { }
+        //
+        // Use TestInitialize to run code before running each test 
+        // [TestInitialize()]
+        // public void MyTestInitialize() { }
+        //
+        // Use TestCleanup to run code after each test has run
+        // [TestCleanup()]
+        // public void MyTestCleanup() { }
+        //
+        #endregion
+
+        [TestMethod]
+        public void Product_create_product()
+        {
+            //Setup
+            Product productNotNullMultiDeal;
+            Product productNullMultiDeal;
+
+            // Act
+            productNotNullMultiDeal = new Product("A", 50, new MultiDeal());
+            productNullMultiDeal = new Product("C", 20);
+
+            // Assert
+            Assert.IsNotNull(productNotNullMultiDeal);
+            Assert.IsNotNull(productNullMultiDeal);
+        }
+
+        [TestMethod]
+        public void Product_retrieve_product_sku_test()
+        {
+            // Setup
+            string sku = "A";
+
+            // Act
+            Product product = new Product(sku, 10);
+
+            // Assert
+            StringAssert.Equals(product.Sku, sku);
+        }
+
+        [TestMethod]
+        public void Product_retrieve_product_price_test()
+        {
+            // Setup
+            int price = 50;
+
+            // Act
+            Product product = new Product("A", price);
+
+            // Assert
+            Assert.IsTrue(product.Price.Equals(price));
+        }
+
+        [TestMethod]
+        public void Product_retrieve_product_offer_test()
+        {
+            // Setup
+            Product productNotNullMultiDeal;
+            Product productNullMultiDeal;
+
+            // Act
+            productNotNullMultiDeal = new Product("A", 50, new MultiDeal());
+            productNullMultiDeal = new Product("C", 20);
+
+            // Assert
+            Assert.IsInstanceOfType(productNotNullMultiDeal.MultiDeal, typeof(MultiDeal));
+            Assert.IsNotNull(productNotNullMultiDeal.MultiDeal);
+
+            Assert.IsInstanceOfType(productNullMultiDeal.MultiDeal, typeof(MultiDeal));
+            Assert.IsNotNull(productNullMultiDeal.MultiDeal);
+        }
+
+        [TestMethod]
+        public void Product_check_product_offer_is_valid_test()
+        {
+            // Setup
+            TimeSpan interval = new TimeSpan(2, 0, 0, 0);
+            DateTime lastWeek = DateTime.Now.Subtract(interval);
+            DateTime nextWeek = DateTime.Now.Add(interval);
+            DateTime now = DateTime.Now;
+
+            // A valid MultiDeal (for now)
+            MultiDeal multiDeal = new MultiDeal(3, 130, lastWeek, nextWeek);
+
+            Product product_1 = new Product("A", 50, multiDeal);
+            Product product_2 = new Product("A", 50, new MultiDeal());
+
+            // Act
+            // Assert
+            Assert.IsTrue(product_1.IsMultiDealValid(now));
+            Assert.IsFalse(product_2.IsMultiDealValid(now));
+
+            // Invalid current times for valid MultiDeal
+            Assert.IsFalse(product_1.IsMultiDealValid(lastWeek.Subtract(interval)));
+            Assert.IsFalse(product_1.IsMultiDealValid(nextWeek.Add(interval)));
+        }
+
+        [TestMethod]
+        public void Product_check_product_price_validation()
+        {
+            // Setup
+            // Act
+            // Assert
+            try
+            {
+                Product product = new Product("A", 0);
+                Assert.Fail("Fail: price can't be less than one, '0' given.");
+            }
+            catch (Exception e)
+            {
+                // Test passes.
+            }
+
+            try
+            {
+                Product product = new Product("A", -1);
+                Assert.Fail("Fail: price can't be less than one, '-1' given.");
+            }
+            catch (Exception e)
+            {
+                // Test passes.
+            }
+        }
+    }
+}
